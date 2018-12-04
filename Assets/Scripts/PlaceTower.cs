1d1a4d0b7fb17f570d61072dd89b78eb987be3b3
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-12-04 16:39:13 +0100

    start

---
+++ Assets/Scripts/PlaceTower.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaceTower : MonoBehaviour {
+
+    SpriteRenderer sprite;
+    public GameObject empty;
+
+    private void Start()
+    {
+        sprite = empty.GetComponent<SpriteRenderer>();
+    }
+
+    public void Selected()
+    {
+        sprite.color.grays
+    }
+
+    public void DeSelected()
+    {
+
+    }
+}
