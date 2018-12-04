b99ce7c29272bd82f4f43f85c3eb8c6d924c7d6f
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-12-04 20:24:52 +0100

    added location selector script

--- Assets/Scripts/PlaceTower.cs
+++ Assets/Scripts/PlaceTower.cs
@@ -4,21 +4,48 @@ using UnityEngine;
 
 public class PlaceTower : MonoBehaviour {
 
+    private List<GameObject> towers = new List<GameObject>();
     SpriteRenderer sprite;
     public GameObject empty;
 
     private void Start()
     {
         sprite = empty.GetComponent<SpriteRenderer>();
+        foreach (Transform child in transform)
+        {
+            if(child.tag == "tower")
+            {
+                towers.Add(child.gameObject);
+            }
+        }
+
     }
 
     public void Selected()
     {
-        sprite.color.grays
+        Color tmp = sprite.color;
+        tmp.a = 0.5f;
+        sprite.color = tmp;
     }
 
     public void DeSelected()
     {
+        Color tmp = sprite.color;
+        tmp.a = 1f;
+        sprite.color = tmp;
+    }
+
+    public void place(string type)
+    {
+        empty.SetActive(false);
+        foreach(GameObject tower in towers)
+        {
+            if(tower.name == type)
+            {
+                tower.SetActive(true);
+                break;
+            }
+        }
 
     }
 }
---
+++ Assets/Scripts/TowerLocationSelector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerLocationSelector : MonoBehaviour {
+
+    List<GameObject> places;
+    private int active = 0;
+    private int change = 0;
+
+	void Start () {
+        places = new List<GameObject>(GameObject.FindGameObjectsWithTag("TowerSpot"));
+    }
+
+    void FixedUpdate()
+    {
+        if (places.Count != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                change = 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                change = -1;
+            }
+            if (change != 0)
+            {
+                places[active].GetComponent<PlaceTower>().DeSelected();
+                active += change;
+                if (active <= 0)
+                {
+                    active = places.Count;
+                }
+
+                if (active >= places.Count)
+                {
+                    active = 0;
+                }
+                places[active].GetComponent<PlaceTower>().Selected();
+                change = 0;
+            }
+
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                places[active].GetComponent<PlaceTower>().place("archer");
+                places.RemoveAt(active);
+                active = 0;
+            }
+        }
+    }
+
+}
