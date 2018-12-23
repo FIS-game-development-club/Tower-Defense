28dba825284ae2b6b50aff4eed4bfd9496ad9824
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-12-23 17:54:36 +0100

    edits

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour {
+
+    public GameObject enemy;
+    public int waves;
+    public int enemyPerWave;
+    public float waveTime;
+    public float PrepareTime;
+    private float LastWave = 0;
+
+	void Start () {
+        LastWave = waveTime - PrepareTime;
+	}
+	
+	void Update () {
+		
+	}
+
+    void StartWave()
+    {
+        for(int i = 0; i <= enemyPerWave; i++)
+        {
+            GameObject newEnemy = Instantiate(enemy, gameObject.transform.position, new Quaternion());
+
+        }
+    }
+}
