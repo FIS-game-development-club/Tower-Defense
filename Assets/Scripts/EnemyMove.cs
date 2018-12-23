044c130d7d92776786739b4c31cce7e168e781f1
Karsten van der Deijl <karsten_vanderdeijl@fis.edu>
2018-12-23 18:29:29 +0100

    created enemy movement
    - enemy movement basics added
    - path (made out of points) created
    - polishment to path, and movement script stille required

---
+++ Assets/Scripts/EnemyMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMove : MonoBehaviour {
+
+    private List<Transform> path = new List<Transform>();
+    public GameObject Path;
+    private int current = 0;
+    public float speed;
+
+
+    void Start()
+    {
+        foreach (Transform child in Path.transform)
+        {
+            path.Add(child);
+        }
+        Debug.Log(path);
+    }
+
+    void FixedUpdate () {
+        float step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, path[current].position, step);
+
+        if(transform.position == path[current].position && current < path.Count -1)
+        {
+            current++;
+        }
+    }
+}
